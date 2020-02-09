7460a86ba548e79139415df1dcd14365fb4b4d07
Diego Dario <diegodario88@gmail.com>
2020-02-09 17:32:18 -0300

    added SingleRoom page and Styled Components

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,11 +1,44 @@
 import React, { Component } from 'react'
+import {Link} from 'react-router-dom'
+
+import defaultBcg from '../images/room-1.jpeg'
+import StyledHero from '../components/StyledHero'
+import Banner from '../components/Banner'
+import { RoomContext } from '../context'
+
 
 export default class SingleRoom extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            slug: this.props.match.params.slug,
+            defaultBcg
+        }
+    }
+
+static contextType = RoomContext
+
     render() {
-        return (
-            <div>
-                hello from single room Page Component
+
+        const { getRoom } = this.context
+        const room = getRoom(this.state.slug)
+        if (!room) {
+            return <div className='error'>
+                <h3> nenhum quarto encontrado... </h3>
+                <Link to='/rooms' className='btn-primary'> voltar? </Link>
             </div>
+        }
+        const { name, description, capacity, size, 
+            price, extras, breakfast, pets, images } = room
+        
+            return (
+            <StyledHero img={images[0] || this.state.defaultBcg}>
+                <Banner title={`quarto ${name}`}>
+                <Link to='/rooms' className='btn-primary'>Ver outros quartos</Link>
+                </Banner>
+              
+            </StyledHero>
         )
     }
 }
